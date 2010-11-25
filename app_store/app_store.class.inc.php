f3b549c70d4c721c8d8c7af342fa9a88d6283279
Kenneth Auchenberg <kenneth@auchenberg.dk>
2010-11-25 11:42:32 +0100

    Added backend hooks to install-app-flow in AppStore

--- app_store/app_store.class.inc.php
+++ app_store/app_store.class.inc.php
@@ -7,12 +7,12 @@ class AppStoreAPI {
   }
   
   public function getTopApps($locale, $limit = 5) {
-    if ($response = $this->podio->request('/app_store/top/'.$locale.'/v2', array('limit' => $limit, 'offset' => 0))) {
+    if ($response = $this->podio->request('/app_store/top/'.$locale.'/v2/', array('limit' => $limit, 'offset' => 0))) {
       return json_decode($response->getBody(), TRUE);
     }
   }
   public function getAppsByCategory($locale, $category, $limit = 100, $offset = 0) {
-    if ($response = $this->podio->request('/app_store/'.$category.'/'.$locale.'/v2', array('limit' => $limit, 'offset' => $offset))) {
+    if ($response = $this->podio->request('/app_store/'.$category.'/'.$locale.'/v2/', array('limit' => $limit, 'offset' => $offset))) {
       return json_decode($response->getBody(), TRUE);
     }
   }
@@ -45,9 +45,15 @@ class AppStoreAPI {
       return json_decode($response->getBody(), TRUE);
     }
   }
-  
+
+  public function getSharedApp($share_id) {
+    if ($response = $this->podio->request('/app_store/'.$share_id)) {
+      return json_decode($response->getBody(), TRUE);
+    }
+  }
+    
   public function shareApp($app_id, $abstract, $description, $language, $category_ids, $file_ids) {
-    if ($response = $this->request('/app_store/', array('app_id' => $app_id, 'abstract' => $abstract, 'description' => $description, 'language' => $language, 'category_ids'=> $category_ids, 'file_ids' => $file_ids ), HTTP_Request2::METHOD_POST)) {
+    if ($response = $this->podio->request('/app_store/', array('app_id' => $app_id, 'abstract' => $abstract, 'description' => $description, 'language' => $language, 'category_ids'=> $category_ids, 'file_ids' => $file_ids ), HTTP_Request2::METHOD_POST)) {
       return json_decode($response->getBody(), TRUE);
     }
   }
