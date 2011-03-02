8e3593b34e15fe2a02187120712b017d2edd1d65
Andreas Haugstrup Pedersen <haugstrup@podio.com>
2011-03-02 14:28:34 +0100

    Adding stream v2 methods

--- stream/stream.class.inc.php
+++ stream/stream.class.inc.php
@@ -12,6 +12,74 @@ class PodioStreamAPI {
   public function __construct() {
     $this->podio = PodioBaseAPI::instance();
   }
+  
+  /**
+   * Returns the global stream. The types of objects in the stream can 
+   * be either "item", "status" or "task". See API documentation 
+   * for details.
+   *
+   * @param $limit The number of stream objects to get. Defaults to 20
+   * @param $offset How far should the objects be offset, defaults to 0
+   * @param $latest The date and time that all events should be before, 
+   *                defaults to no limit
+   *
+   * @return An array of stream objects
+   */
+  public function getGlobal($limit = 20, $offset = 0, $latest = NULL) {
+    $data = array('limit' => $limit, 'offset' => $offset);
+    if ($latest) {
+      $data['latest'] = $latest;
+    }
+    if ($response = $this->podio->request('/stream/v2/', $data)) {
+      return json_decode($response->getBody(), TRUE);
+    }
+  }
+
+  /**
+   * Returns an organisation stream. The types of objects in the stream can 
+   * be either "item", "status" or "task". See API documentation 
+   * for details.
+   *
+   * @param $org_id The id of the organisation to get a stream for
+   * @param $limit The number of stream objects to get. Defaults to 20
+   * @param $offset How far should the objects be offset, defaults to 0
+   * @param $latest The date and time that all events should be before, 
+   *                defaults to no limit
+   *
+   * @return An array of stream objects
+   */
+  public function getOrg($org_id, $limit = 20, $offset = 0, $latest = NULL) {
+    $data = array('limit' => $limit, 'offset' => $offset);
+    if ($latest) {
+      $data['latest'] = $latest;
+    }
+    if ($response = $this->podio->request('/stream/org/'.$org_id.'/v2/', $data)) {
+      return json_decode($response->getBody(), TRUE);
+    }
+  }
+
+  /**
+   * Returns a space stream. The types of objects in the stream can 
+   * be either "item", "status" or "task". See API documentation 
+   * for details.
+   *
+   * @param $space_id The id of the space to get a stream for
+   * @param $limit The number of stream objects to get. Defaults to 20
+   * @param $offset How far should the objects be offset, defaults to 0
+   * @param $latest The date and time that all events should be before, 
+   *                defaults to no limit
+   *
+   * @return An array of stream objects
+   */
+  public function getSpace($space_id, $limit = 20, $offset = 0, $latest = NULL) {
+    $data = array('limit' => $limit, 'offset' => $offset);
+    if ($latest) {
+      $data['latest'] = $latest;
+    }
+    if ($response = $this->podio->request('/stream/space/'.$space_id.'/v2/', $data)) {
+      return json_decode($response->getBody(), TRUE);
+    }
+  }
 
   /**
    * Get objects for a stream. This includes items and statuses with comments, 
@@ -59,6 +127,24 @@ class PodioStreamAPI {
    */
   public function getObject($ref_type = 'status', $ref_id = NULL) {
     $url = '/stream/'.$ref_type.'/'.$ref_id ;
+
+    if ($response = $this->podio->request($url)) {
+      return json_decode($response->getBody(), TRUE);
+    }
+  }
+
+  /**
+   * Returns an object (item or status) as a stream object. This is useful 
+   * when a new status has been posted and should be rendered directly in the 
+   * stream without reloading the entire stream.
+   *
+   * @param $ref_type The type of object. "status" or "item"
+   * @param $ref_id Either the status id or the item id
+   *
+   * @return A single stream object
+   */
+  public function getObjectV2($ref_type = 'status', $ref_id = NULL) {
+    $url = '/stream/'.$ref_type.'/'.$ref_id.'/v2';
 
     if ($response = $this->podio->request($url)) {
       return json_decode($response->getBody(), TRUE);
