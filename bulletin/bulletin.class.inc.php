69561e5543ee5b36c5e9546bf7b38d0f71bd7e8c
Andreas Haugstrup Pedersen <haugstrup@podio.com>
2011-05-09 10:17:36 +0200

    Remove unused methods

--- bulletin/bulletin.class.inc.php
+++ bulletin/bulletin.class.inc.php
@@ -38,35 +38,4 @@ class PodioBulletinAPI {
       return json_decode($response->getBody(), TRUE);
     }
   }
-  
-  /**
-   * Creates a new bulletin with the given title, summary and text. When the 
-   * bulletin is created, all users will be notified of the new bulletin.
-   *
-   * @param $title The title of the new bulletin
-   * @param $summary The summary of the new bulletin
-   * @param $text The main body of the new bulletin
-   */
-  public function create($title, $summary, $text) {
-    $data = array('title' => $title, 'summary' => $summary, 'text' => $text);
-    if ($response = $this->podio->request('/bulletin/', $data, HTTP_Request2::METHOD_POST)) {
-      return json_decode($response->getBody(), TRUE);
-    }
-  }
-  
-  /**
-   * Updates a bulletin to correct mistakes.
-   *
-   * @param $bulletin_id The id of the bulletin to retrieve
-   * @param $title The title of the new bulletin
-   * @param $summary The summary of the new bulletin
-   * @param $text The main body of the new bulletin
-   */
-  public function update($bulletin_id, $title, $summary, $text) {
-    $data = array('title' => $title, 'summary' => $summary, 'text' => $text);
-    if ($response = $this->podio->request('/bulletin/'.$bulletin_id, $data, HTTP_Request2::METHOD_PUT)) {
-      return json_decode($response->getBody(), TRUE);
-    }
-  }
-  
 }
