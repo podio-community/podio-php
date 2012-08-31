95d89754d6289484f9e56af16ff993c511aaf655
Andreas Haugstrup Pedersen <haugstrup@podio.com>
2012-08-31 10:59:07 -0700

    Adding documentation references

--- models/PodioHook.php
+++ models/PodioHook.php
@@ -1,6 +1,9 @@
 <?php
 namespace Podio;
 
+/**
+ * @see https://developers.podio.com/doc/hooks
+ */
 class Hook extends Object {
   public function __construct($attributes = array()) {
     $this->property('hook_id', 'integer');
@@ -15,6 +18,9 @@ class Hook extends Object {
     $this->init($attributes);
   }
 
+  /**
+   * @see https://developers.podio.com/doc/hooks/get-hooks-215285
+   */
   public static function get($ref_type, $ref_id) {
     if ($response = self::podio()->get('/hook/'.$ref_type.'/'.$ref_id.'/')) {
       return self::listing(json_decode($response->getBody(), TRUE));
