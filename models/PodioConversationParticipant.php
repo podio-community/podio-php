3a1285fa4ef52af88ae378bbd7283bff9f8274bc
Andreas Haugstrup Pedersen <haugstrup@podio.com>
2012-09-08 15:05:14 -0700

    Adding Conversation

---
+++ models/PodioConversationParticipant.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @see https://developers.podio.com/doc/conversations
+ */
+class PodioConversationParticipant extends PodioObject {
+  public function __construct($attributes = array()) {
+    $this->property('created_on', 'datetime');
+
+    $this->has_one('user', 'User');
+    $this->has_one('created_via', 'Via');
+    $this->has_one('created_by', 'ByLine');
+
+    $this->init($attributes);
+  }
+
+}
