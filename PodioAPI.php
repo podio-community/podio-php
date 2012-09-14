c6524222630344a49202cefa829749a1c8051c1e
Andreas Haugstrup Pedersen <haugstrup@podio.com>
2012-09-14 09:40:15 -0700

    Adding TaskLabel

--- PodioAPI.php
+++ PodioAPI.php
@@ -35,6 +35,7 @@ require_once 'models/PodioSubscription.php';
 require_once 'models/PodioTag.php';
 require_once 'models/PodioTagSearch.php';
 require_once 'models/PodioTask.php';
+require_once 'models/PodioTaskLabel.php';
 require_once 'models/PodioUser.php';
 require_once 'models/PodioUserMail.php';
 require_once 'models/PodioUserStatus.php';
