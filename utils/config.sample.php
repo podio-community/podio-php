f2f7a79d7bf20cd65b31f649d9606f8b6a280301
Andreas Haugstrup Pedersen <haugstrup@podio.com>
2012-09-17 15:04:49 -0700

    Script for generating reference documentation

---
+++ utils/config.sample.php
@@ -0,0 +1,11 @@
+<?php
+
+define("CLIENT_ID", "");
+define("CLIENT_SECRET", "");
+define("USERNAME", "");
+define("PASSWORD", "");
+
+define('FIELD_ID', 0);
+
+// Make sure errors are output to the screen
+ini_set('display_errors', '1');
