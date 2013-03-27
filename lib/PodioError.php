adaa5bec84fca364c2f58b38e5c72ce72be8378c
Andreas Haugstrup Pedersen <haugstrup@podio.com>
2013-03-26 18:25:45 -0700

    Include more information in exceptions

--- lib/PodioError.php
+++ lib/PodioError.php
@@ -9,11 +9,22 @@ class PodioError extends Exception {
     $this->status = $status;
     $this->url = $url;
     $this->request = $this->body['request'];
-    if (!empty($this->body['error_description'])) {
-      $this->message = $this->body['error_description'];
-    }
   }
 
+  public function __toString() {
+    $str = $str = get_class($this);
+    if (!empty($this->body['error_description'])) {
+      $str .= ': "'.$this->body['error_description'].'"';
+    }
+    $str .= "\nRequest URL: ".$this->request['url'];
+    if (!empty($this->request['query_string'])) {
+      $str .= '?'.$this->request['query_string'];
+    }
+    $str .= "\nRequest Body: ".json_encode($this->request['body']);
+
+    $str .= "\n\nStack Trace: \n".$this->getTraceAsString();
+    return $str;
+  }
 }
 class PodioInvalidGrantError extends PodioError {}
 class PodioBadRequestError extends PodioError {}
