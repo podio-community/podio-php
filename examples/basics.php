24cadd4129621792bb616e0883dacee6bdca010d
Andreas Haugstrup Pedersen <haugstrup@podio.com>
2013-08-25 20:54:50 -0700

    Change example to be items based instead of task based as app auth won't actually work with PodioTask::get()

--- examples/basics.php
+++ examples/basics.php
@@ -17,19 +17,19 @@ if (!Podio::is_authenticated()) {
 }
 
 // You will be working with two types of methods. Direct calls to the API is made using static methods on the classes:
-$task = PodioTask::get(123);
+$item = PodioItem::get(123);
 
-// When possible these calls will return instances of the class in question. You can then work with that object directly. Above we get an instance of the PodioTask class and we can print the task_id and the link to the task. The properties are available directly on the object:
-print $task->id;
-print $task->link;
+// When possible these calls will return instances of the class in question. You can then work with that object directly. Above we get an instance of the PodioItem class and we can print the item_id and the link to the item. The properties are available directly on the object:
+print $item->id;
+print $item->link;
 
 // Open each class in the 'models' folder to see which properties are available for each class.
 
 // Some properties are instances of other classes. You can see these marked as 'has_one' or 'has_many' in the constructor.
-// For example PodioTask has one 'created_by' (instance of PodioByLine) and it has_many 'labels' (array of PodioTaskLabel instances):
-print get_class($task->created_by); // Will print 'PodioByLine'
+// For example PodioItem has one 'created_by' (instance of PodioByLine) and it has_many 'comments' (array of PodioComment instances):
+print get_class($item->created_by); // Will print 'PodioByLine'
 
 // You can then drill into these relationships easily. E.g. to print the name of the author:
-print $task->created_by->name;
+print $item->created_by->name;
 
-// App items are the most complicated part of the Podio API and there are many shortcuts in the PHP library to make your life easier. See items.php for details.
+// App items are the most complicated part of the Podio API and there are many shortcuts in the PHP library to make your life easier. See items.php for more on items.
