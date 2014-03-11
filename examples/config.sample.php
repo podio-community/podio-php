1c90c1cfde17f66032c2a4d17c9aa85a2c39e5d9
Andreas Haugstrup Pedersen <haugstrup@podio.com>
2014-03-10 19:24:19 -0700

    Remove out of date examples and shorten readme to just point to the new documentation pages

--- examples/config.sample.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define("CLIENT_ID", "");
-define("CLIENT_SECRET", "");
-define("USERNAME", "");
-define("PASSWORD", "");
-
-// Make sure errors are output to the screen
-ini_set('display_errors', '1');
