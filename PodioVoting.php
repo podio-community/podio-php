fd55ee8420d6ca0baeff402a31db733fb18fd0b8
Shaun Webb <shaunwebbit@gmail.com>
2014-10-09 01:11:29 +0100

    Update PodioVoting.php

--- PodioVoting.php
+++ PodioVoting.php
@@ -1,24 +1,31 @@
 <?php
-class PodioVoting extends PodioObject
-{
-  public function __construct($attributes = array())
-  {
+/**
+ * @see https://developers.podio.com/doc/voting
+ */
+class PodioVoting extends PodioObject {
+  public function __construct($attributes = array()) {
     $this->property('count', 'integer');
     $this->init($attributes);
   }
   
-  public static function get_result_for_item($item_id, $voting_id)
-  {
+  /**
+   * @see https://developers.podio.com/doc/voting/get-result-of-voting-on-an-item-117727335
+   */
+  public static function get_result_for_item($item_id, $voting_id) {
     return self::member(Podio::get("/voting/item/{$item_id}/voting/{$voting_id}/result"));    
   }
   
-  public static function get_voting_id($app_id)
-  {
+  /**
+   * @see https://developers.podio.com/doc/voting/get-app-votings-117689723
+   */
+  public static function get_voting_id($app_id) {
     return Podio::get("/voting/app/{$app_id}/voting")->json_body();    
   }
   
-  public static function get_list_of_users_with_votes($item_id, $voting_id)
-  {
+  /**
+   * @see https://developers.podio.com/doc/voting/get-list-of-users-with-votes-117729546
+   */
+  public static function get_list_of_users_with_votes($item_id, $voting_id) {
     return Podio::get("/voting/item/{$item_id}/voting/{$voting_id}/user")->json_body();    
   }
   
