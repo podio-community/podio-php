83bc124996a8fec092ba9577e228b2e62fb8667f
k1rked <peterson.kirk@gmail.com>
2019-06-03 11:55:09 -0600

    Update models/PodioItemField.php
    Co-Authored-By: Daniel Schreiber <daniel-schreiber@gmx.de>

--- models/PodioItemField.php
+++ models/PodioItemField.php
@@ -1228,7 +1228,7 @@ class PodioMoneyItemField extends PodioItemField {
 /**
   * Tag Field. Undocumented by Podio, but occurs as unremovable default field "Organization" in contact apps.
   */
-class PodioTagItemField extends PodioItemField{
+class PodioTagItemField extends PodioItemField {
 
     public function api_friendly_values() {
         return $this->values !== null ? $this->values : null;
