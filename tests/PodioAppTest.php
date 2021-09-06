43888fc4cdb7a8b5a7d023f0388eb8593ff968fc
Lucas Michot <lucas@semalead.com>
2021-09-06 17:22:26 +0200

    Move fixture file to a fixture folder.

--- tests/PodioAppTest.php
+++ tests/PodioAppTest.php
@@ -10,7 +10,7 @@ class PodioAppTest extends TestCase
     public function test_performance_large_app(): void
     {
         $start = time();
-        $appString = file_get_contents(__DIR__.'/large-app.json');
+        $appString = file_get_contents(__DIR__.'/fixtures/large-app.json');
         $appJson = json_decode($appString, true);
         new PodioApp(array_merge($appJson, ['__api_values' => true]));
         $duration = time() - $start;
