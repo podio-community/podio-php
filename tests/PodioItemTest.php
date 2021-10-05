c9c8080b92c699fb62df3a54c9666483fc40c98f
Daniel Schreiber <daniel-schreiber@gmx.de>
2021-10-05 10:59:59 +0200

    fixes some exceptions on missing preconditions for save/create operations - relates to #213

---
+++ tests/PodioItemTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Podio\Tests;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use PHPUnit\Framework\TestCase;
+use PodioApp;
+use PodioItem;
+use PodioItemFieldCollection;
+use PodioTextItemField;
+
+class PodioItemTest extends TestCase
+{
+    public function test_create_item(): void
+    {
+        $item = new PodioItem([
+            'app' => new PodioApp(1234),
+            'fields' => new PodioItemFieldCollection([
+                new PodioTextItemField(["external_id" => "title", "values" => "TEST"]),
+            ])
+        ]);
+
+        $this->assertEquals(1234, $item->app->id);
+    }
+
+
+    public function test_save_should_throw_error_if_app_id_missing(): void
+    {
+        $this->expectException('PodioMissingRelationshipError');
+        $item = new PodioItem([
+            'fields' => new PodioItemFieldCollection([
+                new PodioTextItemField(["external_id" => "title", "values" => "TEST"]),
+            ])
+        ]);
+        $item->save();
+    }
+}
