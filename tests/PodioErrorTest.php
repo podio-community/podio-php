9ea391ada9e2248d62dd60f84dced9954f54c3c7
Daniel Schreiber <daniel-schreiber@gmx.de>
2023-07-27 21:11:05 +0200

    fix lint

--- tests/PodioErrorTest.php
+++ tests/PodioErrorTest.php
@@ -7,7 +7,6 @@ use PHPUnit\Framework\TestCase;
 
 class PodioErrorTest extends TestCase
 {
-
     public function test__constructShouldOutputNoWarningForEmptyBody()
     {
         $this->expectOutputString('');
